204f9ac260ba7f581947ffd59c736dfe3ad73e64
ItsukiKigoshi <itsukikigoshi@gmail.com>
2024-03-21 17:22:05 +0900

    Add save_to_csv.py for exporting course information to CSV

---
+++ save_to_csv.py
@@ -0,0 +1,17 @@
+import helper
+import csv
+import datetime
+import os
+
+courses_list = helper.getCourseInfo()
+
+# save to csv
+fieldnames = ['rgno', 'season', 'ay', 'course_no', 'old_cno', 'lang', 'section', 'title_e', 'title_j', 'schedule_meta', 'schedule', 'room', 'comment', 'maxnum', 'instructor', 'unit']
+
+# get date (DDMMYYYY-HHMM) (e.g. 01Jan2022-0000
+date = datetime.datetime.now().strftime("%d%b%Y-%H%M")
+
+with open(f'out/courses_ay{os.environ['ACADEMIC_YEAR']}_asOf{date}.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(courses_list)
